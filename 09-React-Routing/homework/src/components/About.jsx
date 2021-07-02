7716cd3d6d38c750f8228e2dd4da6945c2ba73bb
Edgar Montenegro <edgarmontenegro321@gmail.com>
2021-07-02 01:03:01 -0300

    Module 02 Henry Bootcamp

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About(){
+    return(
+        <div>
+            <h2>Weather app
+                <p>
+                    Created by : Edgar Montenegro
+                </p> 
+            </h2>
+        </div>
+    )
+}
